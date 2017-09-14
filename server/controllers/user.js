2cc0a37646bb1a8091e968033415c3caafeaafee
alfandash <cumiasem91@gmail.com>
2017-09-14 14:15:45 +0700

    release 1: api for user regis and login done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,8 +11,7 @@ const convertPass = require('../helper/convertPass')
 
 
 exports.create = (req, res) => {
-  var secretLength = process.env.SECRET_LENGTH
-  generateSecret(secretLength, (secretKey) => {
+  generateSecret(process.env.SECRET_LENGTH, (secretKey) => {
     var rawPass = {
       pass: `${req.body.password}`,
       secret: `${secretKey}`
@@ -37,4 +36,52 @@ exports.create = (req, res) => {
         })
     })
   })
-} 
+}
+
+exports.signin = (req, res) => {
+  let query = {email: `${req.body.email.toLowerCase()}`}
+  User.find(query)
+  .then((document) => {
+    var rawPass = {
+      pass: req.body.password,
+      secret: document[0].secret
+    }
+    convertPass(rawPass, (hashPassword) => {
+      if (hashPassword === document[0].password) {
+        var payload = {
+          id: `${document[0]._id}`,
+          username: `${document[0].username}`,
+          email: `${document[0].email}`
+        }
+        jwt.login(payload, (token) => {
+          var success = {
+            'success' : {},
+            'message' : 'Login success',
+            'token' : token,
+            'username': payload.username,
+            'email' : payload.email,
+            'id' : payload.id
+          }
+          res.send(success)
+        })
+      } else {
+        var error = {
+          "errors": {},
+          "_message": "Password wrong",
+          "message": "Password wrong",
+          "name": "ValidationError"
+        }
+        res.send(error)
+      }
+    })
+  })
+  .catch((error) => {
+    var error = {
+      'errors': {},
+      '_message': `Cannot find email ${req.body.email}`,
+      'message': `Cannot find email ${req.body.email}`,
+      'name': 'ValidationError'
+    }
+    res.send(error)
+  })
+}
