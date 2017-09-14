b31e2a8d5df9b4df10869f034aca078b57f306ce
alfandash <cumiasem91@gmail.com>
2017-09-14 14:54:43 +0700

    Relese 2:API Articles CRUD, get by cat, get by author done

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -14,3 +14,95 @@ exports.findAll = (req, res) => {
     res.send(error)
   })
 }
+
+exports.findByIdArticle = (req, res) => {
+  var query = {
+    '_id': `${req.params.id}`
+  }
+  Article.find(query)
+  .populate('author')
+  .then(( document ) => {
+    res.send(document[0])
+  })
+  .catch(( error ) => {
+    res.send(error)
+  })
+}
+
+exports.findByAuthor = (req, res) => {
+  var query = {
+    'author': `${req.params.id}`
+  }
+  Article.find(query)
+  .populate('author')
+  .then(( documents ) => {
+    res.send(documents)
+  })
+  .catch(( error ) => {
+    res.send(error)
+  })
+}
+
+exports.findByCategory = (req, res) => {
+  var query = {
+    'category': `${req.params.id}`
+  }
+  Article.find(query)
+  .populate('author')
+  .then(( documents ) => {
+    res.send(documents)
+  })
+  .catch(( error ) => {
+    res.send(error)
+  })
+}
+
+exports.create = (req, res) => {
+  let user = req.locals
+  let add = {
+    title: `${req.body.title}`,
+    content: `${req.body.content}`,
+    category: `${req.body.category}`,
+    author: `${user.id}`
+  }
+  Article.create(add)
+  .then(( response ) => {
+    res.send( response )
+  })
+  .catch(( error ) => {
+    res.send( error )
+  })
+}
+
+exports.update = (req, res) => {
+  let user = req.locals
+  let query = {
+    '_id': `${req.params.id}`
+  }
+  let update = {
+    title: `${req.body.title}`,
+    content: `${req.body.content}`,
+    category: `${req.body.category}`,
+    author: `${user.id}`
+  }
+  Article.update(query, {$set: update})
+  .then(( response ) => {
+    res.send( response )
+  })
+  .catch(( error ) => {
+    res.send( error )
+  })
+}
+
+exports.destroy = (req, res) => {
+  let query = {
+    '_id': `${req.params.id}`
+  }
+  Article.deleteOne(query)
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((error) => {
+    res.send(error)
+  })
+}
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -6,7 +6,11 @@ var articleController = require('../controllers/article')
 var jwt = require('../helper/jwt')
 
 router.get('/', articleController.findAll)
-router.get('/:id')
+router.get('/:id', articleController.findByIdArticle)
+router.get('/author/:id', articleController.findByAuthor)
+router.get('/category/:id', articleController.findByCategory)
 router.post('/', jwt.tokenDecoded, articleController.create)
+router.put('/:id', jwt.tokenDecoded, articleController.update)
+router.delete('/:id', jwt.tokenDecoded, articleController.destroy)
 
 module.exports = router;
